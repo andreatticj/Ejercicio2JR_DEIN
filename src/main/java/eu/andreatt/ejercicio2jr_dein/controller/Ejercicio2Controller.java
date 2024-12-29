50af902f7b2b9b5cd5427197afdb9a2cf3f8a832
andreatticj <andreatticj@gmail.com>
2024-12-29 14:00:15 +0100

    add fxml y esctructura controller

---
+++ src/main/java/eu/andreatt/ejercicio2jr_dein/controller/Ejercicio2Controller.java
@@ -0,0 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private RadioButton informePersonas;
+
+    @FXML
+    private RadioButton informePersonasCalculos;
+
+    @FXML
+    private RadioButton inforomePersoanasSubinformes;
+
+    @FXML
+    private ToggleGroup personas;
+
+    @FXML
+    void abrir(ActionEvent event) {
+
+    }
+
+}
